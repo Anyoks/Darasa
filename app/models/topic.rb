bb37a7976bed85cf0570700e74d6ea566c4c724a
Denn Orina <anyoksdenn@gmail.com>
2015-10-13 12:56:59 +0300

    Fully working in production

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ActiveRecord::Base
 
 	mount_uploader :attachment, PdfUploader #mount the pdf uploader
 
-	validates_presence_of :name, :code, :attachment # make sure these two are entered
+	validates_presence_of :name, :code, :attachment, :unit # make sure these two are entered
 
 	belongs_to :unit
 end
