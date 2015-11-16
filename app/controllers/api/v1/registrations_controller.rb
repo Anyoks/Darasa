f70d1c90f91d53e54d57c088f49016888d936abf
Denn Orina <anyoksdenn@gmail.com>
2015-11-16 10:47:32 +0300

    mobile regisration api controllers :-( I know did my commits terribly here

---
+++ app/controllers/api/v1/registrations_controller.rb
@@ -0,0 +1,20 @@
+class Api::V1::RegistrationsController < ApplicationController
+	skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+	respond_to :json
+	def create
+
+	  user = User.new(user_params)
+	  # byebug
+	  if user.save
+	    render json: user.as_json(auth_token: user.authentication_token, email: user.email), status: :created
+	    return
+	  else
+	    warden.custom_failure!
+	    render json: user.errors, status: :unprocessable_entity
+	  end
+	end
+
+	 def user_params
+      params.require(:user).permit(:first_name, :second_name, :email, :password, :password_confirmation, :provider)
+    end
+end
---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,68 @@
+class Api::V1::SessionsController <  Api::V1::BaseController
+	before_filter :authenticate_user!, except: [:create]
+  before_filter :ensure_user_login_param_exists, only: [:create]
+  after_filter :set_csrf_header, only: [:new, :create]
+  # before_filter :ensure_email_param_exists, only: [:create]
+  # before_filter :ensure_password_param_exists, only: [:create]
+  respond_to :json
+
+  def create
+    resource = User.find_for_database_authentication(email: params[:user_login][:email])
+    return invalid_login_attempt unless resource
+
+    if resource.valid_password?(params[:user_login][:password])
+      sign_in(:user, resource)
+      resource.ensure_authentication_token
+      render json: { success: true, authentication_token: resource.authentication_token, email: resource.email }, status: :created
+      return
+    end
+    invalid_login_attempt
+  end
+
+  def destroy
+    current_user.reset_authentication_token
+    render json: { success: true }, status: :ok
+  end
+
+   def ensure_authentication_token
+     if authentication_token.blank?
+       self.authentication_token = generate_authentication_token
+     end
+  end
+
+
+  protected
+
+  def generate_authentication_token
+    loop do
+      token = Devise.friendly_token
+      break token unless User.where(authentication_token: token).first
+    end
+  end
+
+  def set_csrf_header
+     response.headers['X-CSRF-Token'] = form_authenticity_token
+  end
+
+ 
+  def ensure_user_login_param_exists
+    ensure_param_exists :user_login
+  end
+
+  def ensure_email_param_exists
+    ensure_param_exists :email
+  end
+
+  def ensure_password_param_exists
+    ensure_param_exists :password
+  end
+
+  def ensure_param_exists(param)
+    return unless params[param].blank?
+    render json:{ success: false, message: "Missing #{param} parameter"}, status: :unprocessable_entity
+  end
+
+  def invalid_login_attempt
+    render json: { success: false, message: "Error with your login or password"}, status: :unauthorized
+  end
+end
