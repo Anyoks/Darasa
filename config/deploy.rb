1903138ff05f42ca6cab181b0208d82c608d8259
Denn Orina <anyoksdenn@gmail.com>
2015-11-25 19:01:24 +0300

    set pty to false

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ set :sidekiq_env, 'production'
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-set :pty, true
+set :pty, false
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
