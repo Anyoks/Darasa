54b6f2729b43d4dee2408ae7b66cb927b58f6439
Denn Orina <anyoksdenn@gmail.com>
2015-11-30 14:48:45 +0300

    permitted unit price for unit

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -73,6 +73,6 @@ class UnitsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def unit_params
-      params.require(:unit).permit(:name, :semester_id)
+      params.require(:unit).permit(:name, :semester_id,:price_id)
     end
 end
