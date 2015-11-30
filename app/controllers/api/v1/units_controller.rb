a1b6b5032a2f5b71beda744edfc56d497998d05f
Denn Orina <anyoksdenn@gmail.com>
2015-11-30 13:26:20 +0300

    removed unnecessary comments

--- app/controllers/api/v1/units_controller.rb
+++ app/controllers/api/v1/units_controller.rb
@@ -22,60 +22,10 @@ class Api::V1::UnitsController < ApplicationController
      return invalid_user unless resource
 
      @unit = Unit.find(params[:id])
-    # byebug
-    
-    # @topics = @unit.topics
+ 
   end
 
-  # GET /units/new
-  # def new
-  #   @unit = Unit.new
-  # end
 
-  # # GET /units/1/edit
-  # def edit
-  #  # @semesters = Semester.all #II need to look into this.
-  # end
-
-  # # POST /units
-  # # POST /units.json
-  # def create
-  #   @unit = Unit.new(unit_params)
-
-  #   respond_to do |format|
-  #     if @unit.save
-  #       format.html { redirect_to @unit, notice: 'Unit was successfully created.' }
-  #       format.json { render :show, status: :created, location: @unit }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @unit.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # # PATCH/PUT /units/1
-  # # PATCH/PUT /units/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @unit.update(unit_params)
-  #       format.html { redirect_to units_url, notice: 'Unit was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @unit }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @unit.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # # DELETE /units/1
-  # # DELETE /units/1.json
-  # def destroy
-  #   @unit.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to units_url, notice: 'Unit was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
 
   private
 
