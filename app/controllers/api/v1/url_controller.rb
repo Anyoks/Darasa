36ba687d21c4ab385288fec1b58fe5a5b06a49d8
Denn Orina <anyoksdenn@gmail.com>
2015-11-30 16:30:47 +0300

    refactoring url controller

--- app/controllers/api/v1/url_controller.rb
+++ app/controllers/api/v1/url_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UrlController < ApplicationController
-	before_filter :authenticate_user!
+	before_filter :authenticate_user! , except: [:show]
 	skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 	respond_to :json
 
