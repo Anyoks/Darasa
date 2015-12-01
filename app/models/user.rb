691076db334ddb10b87b3d8022211fca420a64d9
Denn Orina <anyoksdenn@gmail.com>
2015-12-01 09:34:52 +0300

    removed byebug

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,7 +40,7 @@ class User < ActiveRecord::Base
 
 
 	def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
-		byebug
+		# byebug
 		data = access_token.info
 		# byebug
 		user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
