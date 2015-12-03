3ab012a7425ff05008f69123cbcc866735665a52
Denn Orina <anyoksdenn@gmail.com>
2015-12-03 15:27:22 +0300

    refoctored error catching in exams answers controller

--- app/controllers/api/v1/exams_controller.rb
+++ app/controllers/api/v1/exams_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ExamsController < ApplicationController
-  before_filter :authenticate_user!
+  # before_filter :authenticate_user!
   before_filter :ensure_question_id_exists, only: [:answer]
   skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
   respond_to :json
