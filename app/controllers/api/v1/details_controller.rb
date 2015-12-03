ed5a7dbd15bdd1703fd67ad800e2074b8fca3ac3
Denn Orina <anyoksdenn@gmail.com>
2015-12-03 11:26:52 +0300

    added get unit name to remote device requests :-)

--- app/controllers/api/v1/details_controller.rb
+++ app/controllers/api/v1/details_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::DetailsController < ApplicationController
 	# after_filter :set_csrf_header
 	skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 	respond_to :json
+	before_filter :ensure_unit_id_exists, only: [:show_unit]
 
 
 	def show
@@ -28,10 +29,39 @@ class Api::V1::DetailsController < ApplicationController
 		invalid_user
 	end
 
+	def show_unit
+		# ensure_unit_id_exists :unit_id
+
+		resource =  User.find_by_authentication_token(params[:auth_token])
+
+		return invalid_user unless resource
+
+		unit = Unit.find(params[:unit_id])
+
+		# return ensure_param_exists unless unit 
+
+		if unit 
+			unit_name = unit.name
+			render json: { success: true, text: unit_name }, status: :ok
+		else
+			invalid_unit
+		end
+
+	end
+
 	protected
 
 	def ensure_authentication_token_param_exists
 	  ensure_param_exists :authentication_token
+	end
+
+	def ensure_param_exists(param)
+	  return unless params[param].blank?
+	  render json:{ success: false, error: "Missing #{param} parameter"}, status: :unprocessable_entity
+	end
+
+	def ensure_unit_id_exists
+		ensure_param_exists :unit_id
 	end
 
 	# def ensure_param_exists(param)
@@ -42,6 +72,12 @@ class Api::V1::DetailsController < ApplicationController
 	def invalid_user
 		render json: { success: false, message: "Error with your credentials"}, status: :unauthorized
 	end
+
+	def invalid_unit
+		render json: { success: false, error: "Error could not find unit name"}, status: :unauthorized
+	end
+
+
 
 	def set_csrf_header
 	   response.headers['X-CSRF-Token'] = form_authenticity_token
