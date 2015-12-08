9713cf4942f6a95ea909e0a1acc0668f7552c269
Denn Orina <anyoksdenn@gmail.com>
2015-12-08 11:03:18 +0300

    removed authenticate user for sign_up

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::RegistrationsController < ApplicationController
+	before_filter :authenticate_user!, except: [:create]
 	skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 	respond_to :json
 	def create
