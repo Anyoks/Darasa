5a6323650ae43a5eb7fc9cf3caf3d8c5123807a8
Denn Orina <anyoksdenn@gmail.com>
2015-12-08 07:06:11 +0300

    added check for environment to picture model before setting domain url

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -18,8 +18,18 @@
 class Ckeditor::Picture < Ckeditor::Asset
   mount_uploader :data, CkeditorPictureUploader, :mount_on => :data_file_name
 
+  # def set_image_url
+  # 	self.role ||= Role.find_by_name('registered') 
+  # end
+
   def url_content
+
+  	if Rails.env["development"]
+		url(:content)
+  	else
   	domain = "http://darasa.co.ke"
     "#{domain}#{url(:content)}"
+	end
+
   end
 end
