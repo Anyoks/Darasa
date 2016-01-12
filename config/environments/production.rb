760f0bfcb94480a979340375424d46916828b004
Denn Orina <anyoksdenn@gmail.com>
2016-01-12 13:20:14 +0300

    added ckeditor.rake and added asset precompilation code in production.rb for ckeditor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+  config.assets.precompile += Ckeditor.assets
+  config.assets.precompile += %w(ckeditor/*)
 end
