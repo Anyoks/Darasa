376662dbedae3d8df7ecf771e41a7b6b9ef72d7f
Denn Orina <anyoksdenn@gmail.com>
2016-01-18 13:58:01 +0300

    added ckeditor precompile commands to the application config file

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,10 @@ module DarasaApp
     # config.i18n.default_locale = :de
     config.autoload_paths += %W(#{config.root}/lib)
 
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
