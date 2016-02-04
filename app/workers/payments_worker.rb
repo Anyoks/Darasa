70d0a1703fb9300d515f9fde152124e944f5d706
Denn Orina <anyoksdenn@gmail.com>
2016-02-04 14:56:32 +0300

    turned off the Pesapal payments worker, He is now dead, asleep: gone!

--- app/workers/payments_worker.rb
+++ app/workers/payments_worker.rb
@@ -1,45 +1,45 @@
-class PaymentsWorker
-	include Sidekiq::Worker
-	sidekiq_options queue: :update_payment_status
+# class PaymentsWorker
+# 	include Sidekiq::Worker
+# 	sidekiq_options queue: :update_payment_status
 
-	def perform
-		Payment.where(status: 'PENDING').each do |payment|
-			# exception_catcher {
-				merchant_reference = payment.pesapal_merchant_reference # received from pesapal
-				transaction_id = payment.pesapal_transaction_tracking_id # received from pesapal
+# 	def perform
+# 		Payment.where(status: 'PENDING').each do |payment|
+# 			# exception_catcher {
+# 				merchant_reference = payment.pesapal_merchant_reference # received from pesapal
+# 				transaction_id = payment.pesapal_transaction_tracking_id # received from pesapal
 
-				#create the url to check
-				url = Pesapal::QueryPaymentStatus.new(merchant_reference, transaction_id, true).url
+# 				#create the url to check
+# 				url = Pesapal::QueryPaymentStatus.new(merchant_reference, transaction_id, true).url
 
-				#fetch status
-				response = HTTParty.get(url) 
+# 				#fetch status
+# 				response = HTTParty.get(url) 
 
-				if response.code == 200
-					status = response.body.split("=")[1] if response.body.split('=')[0] == 'pesapal_response_data'
+# 				if response.code == 200
+# 					status = response.body.split("=")[1] if response.body.split('=')[0] == 'pesapal_response_data'
 
-					if status && status != payment.status
-						payment.status = status
-						payment.order_url =  nil
+# 					if status && status != payment.status
+# 						payment.status = status
+# 						payment.order_url =  nil
 
-						payment.save!
-					end
-				end
-			# }
-		end
-	end
+# 						payment.save!
+# 					end
+# 				end
+# 			# }
+# 		end
+# 	end
 
-	#Lets Log it. :-)
-	def self.rake_logger
-		@@rake_logger ||= Logger.new("#{Rails.root}/log/rake_logger.log", 'monthly')
-	end
+# 	#Lets Log it. :-)
+# 	def self.rake_logger
+# 		@@rake_logger ||= Logger.new("#{Rails.root}/log/rake_logger.log", 'monthly')
+# 	end
 
-	def self.exception_catcher
-		begin
-			yield
-		rescue Exception => err
-			rake_logger.info("E [#{Time.now.utc.iso8601}] ERROR: #{err.message}")
-		end
-	end
+# 	def self.exception_catcher
+# 		begin
+# 			yield
+# 		rescue Exception => err
+# 			rake_logger.info("E [#{Time.now.utc.iso8601}] ERROR: #{err.message}")
+# 		end
+# 	end
 
 
-end
+# end
--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,15 +2,15 @@ require 'rufus-scheduler'
 
 # Let's use the rufus-scheduler singleton
 #
-payment_checker = Rufus::Scheduler.singleton
+# payment_checker = Rufus::Scheduler.singleton
 
 
 # Stupid recurrent task...
 #
-payment_checker.every '1m' do
+# payment_checker.every '1m' do
 
 	#Now call my payment chcecker workeer here to do some hard work. HardWorker.perform_async
 	# PaymentsWorker.perform_async
 
 
-end
+# end
