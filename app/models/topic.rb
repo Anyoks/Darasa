0fb5b23f28c2585183196f8d41f5934679a01477
Denn Orina <anyoksdenn@gmail.com>
2016-02-09 16:01:57 +0300

    removed code from topic and from model

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -15,7 +15,7 @@ class Topic < ActiveRecord::Base
 
 	# mount_uploader :attachment, PdfUploader #mount the pdf uploader
 
-	validates_presence_of :name, :code, :unit # make sure these two are entered
+	validates_presence_of :name, :unit # make sure these two are entered
 
 	belongs_to :unit
 
