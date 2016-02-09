6076cbda00e30a73574c2309859a35f408868fe2
Denn Orina <anyoksdenn@gmail.com>
2016-02-09 08:12:55 +0300

    Removed mpesa_code not null enforcement, causes the migration to fail on production. will fix this later

--- db/migrate/20160204124635_add_uniqueness_to_payments.rb
+++ db/migrate/20160204124635_add_uniqueness_to_payments.rb
@@ -1,6 +1,6 @@
 class AddUniquenessToPayments < ActiveRecord::Migration
   def change
   	add_index :payments, :mpesa_code,  unique: true
-  	change_column :payments, :mpesa_code, :string, :null => false
+  	change_column :payments, :mpesa_code, :string#, :null => false
   end
 end
