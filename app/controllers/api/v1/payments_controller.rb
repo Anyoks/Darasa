379e1dc252fd532a77b211bee03e5c197d603ceb
Denn Orina <anyoksdenn@gmail.com>
2016-02-29 20:46:52 +0300

    fixed syntax error in payments

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -33,7 +33,7 @@ class Api::V1::PaymentsController < ApplicationController
 			if cash.amount.to_i >= 50 #i'll have to change this to seperate the large payments from the smaller ones
 			#check if it has been used i.e it is in the payments
 				if @payment.save
-					unless resource.has_admin_previlages?
+					unless user.has_admin_previlages?
 						log_payment_activity user, topic_name, "#{user.first_name} successfully purchased this topic, #{topic.name}"
 					end
 
@@ -42,7 +42,7 @@ class Api::V1::PaymentsController < ApplicationController
 					@failed_payment = FailedPayment.new(payment_params)
 					@failed_payment.save
 
-					unless resource.has_admin_previlages?
+					unless user.has_admin_previlages?
 						log_payment_activity user, topic_name,  "#{user.first_name} just attempted to purchase this topic with a fake or duplicate Mpesa code, #{topic.name}"
 					end
 
@@ -53,7 +53,7 @@ class Api::V1::PaymentsController < ApplicationController
 				@failed_payment.save
 				@failed_payment.update_attribute(:amount, cash.amount)
 
-				unless resource.has_admin_previlages?
+				unless user.has_admin_previlages?
 					log_payment_activity user, topic_name, "#{user.first_name} paid less while purchasing this topic, #{topic.name}"
 				end
 
@@ -63,7 +63,7 @@ class Api::V1::PaymentsController < ApplicationController
 			@failed_payment = FailedPayment.new(payment_params)
 			@failed_payment.save
 
-			unless resource.has_admin_previlages?
+			unless user.has_admin_previlages?
 				log_payment_activity user, topic_name,"#{user.first_name} just attempted to purchase this topic with an Mpesa code that is not in our Database, #{topic.name}"
 			end
 			
