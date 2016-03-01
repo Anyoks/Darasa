4551b27bb4962f554a7b14e5fa77573328a98742
Denn Orina <anyoksdenn@gmail.com>
2016-03-01 08:43:15 +0300

    Removing duplicate mpesa texts

--- app/models/sms.rb
+++ app/models/sms.rb
@@ -68,6 +68,18 @@ class Sms < ActiveRecord::Base
 		return hash
 	end
 
+	def self.dedupe
+	    # find all models and group them on keys which should be common
+	    grouped = all.group_by{|sms| [ sms.mpesa_code ] }
+	    grouped.values.each do |duplicates|
+	      # the first one we want to keep right?
+	      first_one = duplicates.shift # or pop for last one
+	      # if there are any more left, they are duplicates
+	      # so delete all of them
+	      duplicates.each{|double| double.destroy} # duplicates can now be destroyed
+	    end
+	  end
+
 
 
 
