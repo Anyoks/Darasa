0d8c6de5a4e475f7d0f543c04b8ae19a4cd4ac95
Denn Orina <anyoksdenn@gmail.com>
2016-03-01 09:29:18 +0300

    Still trying to remove dup records, previous method was killed by system

--- app/models/sms.rb
+++ app/models/sms.rb
@@ -78,7 +78,15 @@ class Sms < ActiveRecord::Base
 	      # so delete all of them
 	      duplicates.each{|double| double.destroy} # duplicates can now be destroyed
 	    end
-	  end
+	end
+
+	def self.deduplicate
+		duplicate_row_values = Sms.select('mpesa_code count(*)').group('mpesa_code').having('count(*) > 1').pluck('mpesa_code')
+
+		duplicate_row_values.each do |mpesa_code|
+			Sms.where(mpesa_code: mpesa_code).order(id: :desc)[1..-1].map(&:destroy)
+		end
+	end
 
 
 
