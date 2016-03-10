99d2bd5b1996bf4f404a08949a0c10c98493c35c
Denn Orina <anyoksdenn@gmail.com>
2016-03-10 21:29:04 +0300

    The is the Question core! We can now view all questions, edit each individual one, destroy view it's answer etc

--- app/models/question.rb
+++ app/models/question.rb
@@ -13,20 +13,18 @@
 class Question < ActiveRecord::Base
 	# require 'json'
 	acts_as_paranoid
-	
+	validates_presence_of :question,:subtopic_id
+
 	belongs_to :exam
 
 	belongs_to :subtopic
 
 	has_one :response, :dependent => :destroy
 
-	accepts_nested_attributes_for :response#, :reject_if => lambda { |a| a[:response].blank? }, :allow_destroy => true
+	accepts_nested_attributes_for :response, reject_if: proc { |attributes| attributes['answer'].blank? }, :allow_destroy => true
 
-	# def as_json(options={})
-	# 	# question = raw Question.find_by("exam_id", 25).question
- #      super(:only => [:id, :question]
- #      )
- #    end
+
  	#lOGGIN USER ACTIVITIES
 	has_many :question_activities, :dependent => :destroy
+
 end
