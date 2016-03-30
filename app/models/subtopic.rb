bdf4b7c5e60ae147c108016c6a5d693d6b1c0213
Denn Orina <anyoksdenn@gmail.com>
2016-03-30 12:28:16 +0300

    annotated Questions and Subtopics

--- app/models/subtopic.rb
+++ app/models/subtopic.rb
@@ -2,16 +2,12 @@
 #
 # Table name: subtopics
 #
-#  id                :uuid             not null, primary key
-#  title             :text
-#  created_at        :datetime         not null
-#  updated_at        :datetime         not null
-#  topic_id          :uuid
-#  deleted_at        :datetime
-#  html_file_name    :string
-#  html_content_type :string
-#  html_file_size    :integer
-#  html_updated_at   :datetime
+#  id         :uuid             not null, primary key
+#  title      :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  topic_id   :uuid
+#  deleted_at :datetime
 #
 
 class Subtopic < ActiveRecord::Base
--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -2,17 +2,13 @@
 #
 # Table name: questions
 #
-#  question          :text
-#  created_at        :datetime         not null
-#  updated_at        :datetime         not null
-#  id                :uuid             not null, primary key
-#  exam_id           :uuid
-#  subtopic_id       :uuid
-#  deleted_at        :datetime
-#  html_file_name    :string
-#  html_content_type :string
-#  html_file_size    :integer
-#  html_updated_at   :datetime
+#  question    :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  id          :uuid             not null, primary key
+#  exam_id     :uuid
+#  subtopic_id :uuid
+#  deleted_at  :datetime
 #
 
 require 'test_helper'
