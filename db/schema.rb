e6179e18e15869e3bf0047b0ce8f99cef6bf407f
Denn Orina <anyoksdenn@gmail.com>
2016-03-30 14:52:45 +0300

    Enable hstore extension, now wer can store hashes into the database

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330111208) do
+ActiveRecord::Schema.define(version: 20160330113819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
+  enable_extension "hstore"
 
   create_table "campus", id: :uuid, default: "uuid_generate_v4()", force: :cascade do |t|
     t.string   "campus_name"
