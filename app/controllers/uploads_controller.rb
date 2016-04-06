0a7b9f76f0e37013bc477e7addf18f3b76717a83
Denn Orina <anyoksdenn@gmail.com>
2016-04-06 09:47:44 +0300

    removed image saving module on doc_bot for testing purposes

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -108,22 +108,22 @@ class UploadsController < ApplicationController
 
       image_name = img.attributes.first[1].value # img name
 
-      image_file = Ckeditor.picture_model.new
+      # image_file = Ckeditor.picture_model.new
 
-      file = File.open("/home/orinamokaya/Desktop/one/Darasa dev files/Real documents/tax edited/solution/solution/#{image_name}")
+      # file = File.open("/home/orinamokaya/Desktop/one/Darasa dev files/Real documents/tax edited/solution/solution/#{image_name}")
 
-      image_file.data = file
-      image_file.save!
+      # image_file.data = file
+      # image_file.save!
 
        
      
         # question << image
 
-        if Rails.env["development"]
-            img.attributes.first[1].value = image_file.url
-        else
-            img.attributes.first[1].value = image_file.url
-        end
+        # if Rails.env["development"]
+        #     img.attributes.first[1].value = image_file.url
+        # else
+        #     img.attributes.first[1].value = image_file.url
+        # end
         # question <<   img.attributes.first[1].value
     end
 
