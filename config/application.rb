fe9a3dd685935438687d500394f078c9826a20ec
Denn Orina <anyoksdenn@gmail.com>
2016-04-12 15:05:24 +0300

    compressed json responses and requests for better performance in slow internet connections

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,11 @@ module DarasaApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    #
+    #Compress Json requests for faster performance in slow internet connections.
+    config.middleware.use Rack::Deflater
+
+
     config.autoload_paths += %W(#{config.root}/lib)
 
     config.assets.precompile += Ckeditor.assets
--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -9,3 +9,13 @@ ActionDispatch::ParamsParser::DEFAULT_PARSERS[Mime::JSONAPI] = -> body { JSON.pa
 }
 
 # Mime::Type.register "text/html", :html
+
+# config/initializers/mime_types.rb
+
+#decompress compressed Json requests
+
+Mime::Type.register "gizp/json", :gzipjson
+ActionDispatch::ParamsParser::DEFAULT_PARSERS[Mime::Type.lookup('gzip/json')]=lambda do |raw_body|
+  body = ActiveSupport::Gzip.decompress(raw_body)
+  JSON.parse(body).with_indifferent_access
+end
