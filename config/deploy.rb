00b7ac94c791118005f936badd52aacbb41f687a
Denn Orina <anyoksdenn@gmail.com>
2016-04-19 16:18:22 +0300

    added public folders to sim linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,8 @@ set :deploy_to, '/home/deploy/darasa'
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :bundle_binstubs, nil
 set :linked_files, %w{config/database.yml config/secrets.yml config/pesapal.yml config/initializers/devise.rb}
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads public/assets/css public/assets/font-awesome public/assets/fonts 
+	public/assets/img public/assets/js public/assets/sass public/css public/favicons public/fonts public/font-awesome public/images public/js}
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
