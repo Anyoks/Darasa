2199a6dce65208c88d1e794183451cf8f670b980
Denn Orina <anyoksdenn@gmail.com>
2016-06-09 15:46:07 +0300

    Lord, Help me

--- app/assets/javascripts/sites.js
+++ app/assets/javascripts/sites.js
@@ -163,7 +163,7 @@
 
 							// Show message.
 								$message._show('success', 'Thank you!');
-								//$message._show('failure', 'Something went wrong. Please try again.');
+								$message._show('failure', 'Something went wrong. Please try again.');
 
 						}, 750);
 
